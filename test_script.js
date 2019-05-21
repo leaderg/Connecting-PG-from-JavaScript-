b9a83b0c19a37cb6bf2cd4a34f524e4527e6cb3f
leaderg <leader.g@gmail.com>
2019-05-21 21:34:03 +0000

    Added knex functionality.

--- test_script.js
+++ test_script.js
@@ -1,35 +1,25 @@
-const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+const knex = require('knex') ({
+  client:'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
 });
 
-let input = process.argv.slice(2)[0];
-
-client.connect((err) => {
-  if (err) {
+knex.select().from('famous_people').asCallback((err, result) => {
+   if (err) {
     return console.error("Connection Error", err);
   }
-  console.log("Searching...");
-  client.query("SELECT * FROM famous_people WHERE first_name = $1", [input], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    console.log(`Found ${result.rows.length} person(s) by the name '${input}'`);
-    for (let entry in result.rows) {
-      let index = Number(entry) + 1;
-      console.log (`- ${index}: ${result.rows[entry].first_name} ${result.rows[entry].last_name}, born ${result.rows[entry].birthdate}`);
-    }
-    client.end();
-  });
+  console.dir(result);
+  // for (let entry in result) {
+  //   console.log(result[entry]);
+  // }
 });
 
-// first_name: 'Paul',
-//     last_name: 'Rudd',
-//     birthdate: 1969-04-06T00:00:00.000Z
+
